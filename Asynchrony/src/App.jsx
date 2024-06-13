7fdc9051302eb2e19d66733747f554a4e3fcf84d
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-06-13 09:21:38 +0200

    Updated App.jsx to fix useState variable name and add error handling to fetch API call

--- Asynchrony/src/App.jsx
+++ Asynchrony/src/App.jsx
@@ -1,13 +1,22 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 
 function App() {
-  const [count, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
  fetch('https://jsonplaceholder.typicode.com/posts')
-  })
+ .then(response => {
+  if (!response.ok) {
+    throw new Error(HTTP error! status: ${response.status});
+  } else {
+    return response.json();
+  }
+})
+.then(data => setPosts(data))
+.catch(error => setError(error.message));
+}, []);
 
 }
 
